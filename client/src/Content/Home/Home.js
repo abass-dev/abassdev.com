17a9550b87919d1244db0973d46d2c8a8adfda19
Abass Ben Cheik <cheikabassben@gmail.com>
2021-11-22 18:48:31 +0000

    feat: customize the background

--- client/src/Content/Home/Home.js
+++ client/src/Content/Home/Home.js
@@ -3,6 +3,7 @@ import Profile from '../Profile/Profile'
 import Skill from '../Skill/Skill'
 import Project from '../Project/Project'
 import Footer from '../Footer/Footer'
+import './Home.css'
 
 export default function Home() {
     return (
@@ -10,11 +11,13 @@ export default function Home() {
             <div className='row'>
                 <div className='profile-container'>
                     <Profile />
+                    <div className='home-container'>
                     <Skill />
                     <Project />
                 </div>
                 <Footer />
             </div>
+        </div>
         </div>
     );
 }
