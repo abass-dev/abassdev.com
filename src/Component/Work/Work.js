e6e7611e0ecaffda25c999c4d97dbdfd9fe3c7f7
abass-dev <cheikabassben@gmail.com>
2021-12-07 20:01:19 +0000

    feat(i18n): trans work componenet

--- src/Component/Work/Work.js
+++ src/Component/Work/Work.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import 'font-awesome/css/font-awesome.min.css'
 import './Work.css'
+import { useTranslation } from 'react-i18next'
 
 export default function Work () {
+    const {t, i18n} = useTranslation();
     return (
         <div className='container text-white mb-4 md-title'>
             <div className='row'>
                 <div className='col-12'>
-                <h1 className='large-title fw-bold'>MON TRAVAIL</h1>
+                <h1 className='large-title fw-bold'>{t("work.title")}</h1>
                 </div>
                 <div className='col-md-4 mb-4'>
                     <div className='card work-card-gb border-0'>
@@ -15,11 +17,9 @@ export default function Work () {
                             <p className='card-title text-center'>
                              <span className='work-icon text-warning fa fa-globe'></span>
                              {" "}
-                             <h2>Développement Web</h2>
+                             <h2>{t("work.webdev.title")}</h2>
                             </p>
-                            <p>
-                             Je fournis des services web fonctionnels tels que le développement de sites web personnalisés, le développement de wordpress, les solutions de commerce électronique et bien plus encore.
-                            </p>
+                            <p>{t("work.webdev.description")}</p>
                         </div>
                     </div>
                 </div>
@@ -29,11 +29,9 @@ export default function Work () {
                             <p className='card-title text-center'>
                              <span className='work-icon text-warning fa fa-line-chart'></span>
                              {" "}
-                             <h2>SEO et sécurité</h2>
+                             <h2>{t("work.seo.title")}</h2>
                             </p>
-                            <p>
-                            Je propose d'excellentes solutions en matière de sécurité logicielle et de pratiques de référencement qui augmenteront la quantité et la qualité du trafic vers votre site web.
-                            </p>
+                            <p>{t("work.seo.description")}</p>
                         </div>
                     </div>
                 </div>
@@ -43,11 +41,9 @@ export default function Work () {
                             <p className='card-title text-center'>
                              <span className='work-icon text-warning fa fa-paint-brush'></span>
                              {" "}
-                             <h2>Web Design</h2>
+                             <h2>{t("work.webdesign.title")}</h2>
                             </p>
-                            <p>
-                            Je conçois des sites Web étonnants et intuitifs qui représentent votre marque et l'identité de votre entreprise grâce à un design UI/UX exceptionnel.
-                            </p>
+                            <p>{t("work.webdesign.description")}</p>
                         </div>
                     </div>
                 </div>
