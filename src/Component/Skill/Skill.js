3e02f2ea5691057124b7455c334b5e3c6e23db47
abass-dev <cheikabassben@gmail.com>
2021-12-14 19:56:06 +0000

    feat(component, refactor, UI): - add story component - review the UI - translation missing - incomplet content / TODO in story component

--- src/Component/Skill/Skill.js
+++ src/Component/Skill/Skill.js
@@ -16,10 +16,10 @@ export default function Skill() {
                   <div className='row'>
                     <div className='col-md-6 mb-4'>
                     <h4 className='md-title'>{t("skill.frontend")}</h4>
-                    <div className='card bg-secondary'>
+                    <div className='card border-primary bg-transparent'>
                     <div className='card-body md-title'>
-                    <div className='row skill-card-bg'>
-                     <div className='col-12 skill-card-text'>
+                    <div className='row'>
+                     <div className='col-12 skill-card-text card-text'>
                         <i class="devicon-sass-original colored"></i>
                         <span> Sass</span>
                     </div>
@@ -42,10 +42,10 @@ export default function Skill() {
                     
                     <div className='col-md-6 mb-4'>
                     <h4 className='md-title'>{t("skill.backend")}</h4>
-                    <div className='card bg-warning'>
+                    <div className='card bg-transparent border-primary'>
                     <div className='card-body md-title'>
-                    <div className='row skill-card-bg'>
-                    <div className='col-12 skill-card-text'>
+                    <div className='row'>
+                    <div className='col-12 skill-card-text card-text'>
                         <i class="devicon-php-plain colored"></i>
                         <span> PHP</span>
                     </div>
@@ -69,10 +69,10 @@ export default function Skill() {
                     </div>
                     <div className='col-md-6 mb-4'>
                     <h4 className='md-title'>{t("skill.database")}</h4>
-                    <div className='card bg-success'>
+                    <div className='card bg-transparent border-primary'>
                     <div className='card-body md-title'>
-                        <div className='row skill-card-bg'>
-                        <div className='col-12 skill-card-text'>
+                        <div className='row'>
+                        <div className='col-12 skill-card-text card-text'>
                         <i class="devicon-mongodb-plain colored"></i>
                         <span> MongoDB</span>
                     </div>
@@ -90,10 +90,10 @@ export default function Skill() {
                     </div>
                     <div className='col-md-6 mb-4'>
                     <h4 className='md-title'>{t("skill.tools")}</h4>
-                    <div className='card bg-info'>
+                    <div className='card bg-transparent border-primary'>
                     <div className='card-body md-title'>
-                        <div className='row skill-card-bg'>
-                        <div className='col-12 skill-card-text'>
+                        <div className='row'>
+                        <div className='col-12 skill-card-text card-text'>
                         <i class="devicon-git-plain colored"></i>
                         <span> Git</span>
                     </div>
@@ -112,10 +112,10 @@ export default function Skill() {
                     
                     <div className='col-md-6 mb-4'>
                     <h4 className='md-title'>{t("skill.mobiles")}</h4>
-                    <div className='card bg-secondary'>
+                    <div className='card border-primary bg-transparent'>
                     <div className='card-body md-title'>
-                        <div className='row skill-card-bg'>
-                        <div className='col-12 skill-card-text'>
+                        <div className='row'>
+                        <div className='col-12 skill-card-text card-text'>
                         <i class="devicon-java-plain colored"></i>
                         <span> Java</span>
                         </div>
@@ -130,10 +130,10 @@ export default function Skill() {
                     
                     <div className='col-md-6 mb-4'>
                     <h4 className='md-title'>{t("skill.software")}</h4>
-                    <div className='card bg-danger'>
+                    <div className='card bg-transparent border-primary'>
                     <div className='card-body md-title'>
-                        <div className='row skill-card-bg'>
-                        <div className='col-12 skill-card-text'>
+                        <div className='row'>
+                        <div className='col-12 card-text'>
                         <i class="devicon-python-plain colored"></i>
                         <span> Python</span>
                     </div>
@@ -150,60 +150,7 @@ export default function Skill() {
                   </div>
                 </div>
                 
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>PHP </span> <div style={{ width: '75%' }} className="progress-bar progress-bar-striped bg-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>JAVA </span> <div style={{ width: '45%' }} className="progress-bar progress-bar-striped bg-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>JS </span> <div style={{ width: '65%' }} className="progress-bar progress-bar-striped bg-warning" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">65%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>HTML </span> <div style={{ width: '80%' }} className="progress-bar progress-bar-striped bg-danger" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>PYTHON </span> <div style={{ width: '45%' }} className="progress-bar progress-bar-striped bg-dark" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">60%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>MYSQL </span> <div style={{ width: '55%' }} className="progress-bar progress-bar-striped bg-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">65%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>CSS </span> <div style={{ width: '70%' }} className="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>MONGODB </span> <div style={{ width: '30%' }} className="progress-bar progress-bar-striped bg-warning" role="progressbar" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100">35%</div>
-                    </div>
-                </div>
-
-                <div className='col-md-6 mb-4'>
-                    <div className="progress">
-                        <span className='bg-transparent mx-1 text-dark skill-progress-title'>POSTGRESQL </span> <div style={{ width: '35%' }} className="progress-bar progress-bar-striped bg-info" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">40%</div>
-                    </div>
-                </div>
-
+               
             </div>
         </div>
     );
