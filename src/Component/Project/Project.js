450a396fce49ae4278197bda3190634aa4f17372
Abass Ben Cheick <cheikabassben@gmail.com>
2022-02-12 00:20:19 +0000

    fix the primary color for the cards

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -4,7 +4,7 @@ import projectCode from '../../assets/images/project-nigatedev.jpg'
 import projectTodaydevs from '../../assets/images/projet-todaydevs.jpg'
 import projectPortfolio from '../../assets/images/project-portfolio.jpg'
 import {useTranslation} from 'react-i18next'
-
+import './Project.css'
 export default function Project() {
     const {t} = useTranslation();
     return (
@@ -22,7 +22,7 @@ export default function Project() {
                                 <p className='h2 md-title card-title'>
                                     {t("project.nigatedev.title")}
                                 </p>
-                                <a href="http://github.com/nigatedev/nigatedev" class="btn mt-2 bg-dark text-primary btn-primary-outline btn-sm"><i className='fa fa-github'></i> Code</a>
+                                <a href="http://github.com/nigatedev/nigatedev" class="btn mt-2 card-dark text-primary btn-primary-outline btn-sm"><i className='fa fa-github'></i> Code</a>
                                 {" "}
                                 <a className='text-center' href="https://packagist.org/packages/nigatedev/nigatedev" title="version"><img alt='' src="https://img.shields.io/packagist/v/nigatedev/nigatedev.svg?style=flat-square" /></a>
                                 {" "}
@@ -31,7 +31,7 @@ export default function Project() {
                                 {" "}
                                 <a href="https://github.com/nigatedev/nigatedev/issues"><img alt="Issues" src="https://img.shields.io/github/issues/nigatedev/nigatedev?color=0088ff" /></a>
                             </div>
-                            <div className='card-body bg-dark text-white'>
+                            <div className='card-body card-dark text-white'>
                                 <i className='fw-bold fa fa-code'></i>
                                 {" "}
                                 <span className='fw-lighter'>
@@ -47,12 +47,12 @@ export default function Project() {
 
                     <div className='col-md-6 mb-4'>
                         <div className='card border-secondary'>
-                            <img alt='' className='card-img-top' src={projectTodaydevs} />
+                            <img alt='todaydevs' className='card-img-top' src={projectTodaydevs} />
                             <div class="text-white card-img-overlay">
                                 <p className='h2 md-title text-light card-title'>
                                     {t("project.todaydevs.title")}
                                 </p>
-                                <a href="https://todaydevs.com" class="btn mt-2 bg-dark text-primary btn-primary-outline btn-sm"><i className='fa fa-globe'></i> Site</a>
+                                <a href="https://todaydevs.com" class="btn mt-2 card-dark text-primary btn-primary-outline btn-sm"><i className='fa fa-globe'></i> Site</a>
                             </div>
                             <div className='card-body'>
                                 <i className='fw-bold fa fa-code'></i>
@@ -73,7 +73,7 @@ export default function Project() {
                                 <p className='h2 md-title text-light card-title'>
                                     {t("project.formbuilder.title")}
                                 </p>
-                                <a href="https://github.com/abass-dev/BenOSP-FormBuilder" class="btn mt-2 text-primary btn-primary-outline btn-sm bg-dark"><i className='fa fa-github'></i> Code</a>
+                                <a href="https://github.com/abass-dev/BenOSP-FormBuilder" class="btn mt-2 text-primary btn-primary-outline btn-sm card-dark"><i className='fa fa-github'></i> Code</a>
                                     <a className='text-center' href="https://packagist.org/packages/benosp/formbuilder" title="version"><img alt='' src="https://img.shields.io/packagist/v/benosp/formbuilder.svg?style=flat-square" /></a>
                                 {" "}
                                 <a href="https://github.com/abass-dev/BenOSP-FormBuilder/blob/master/LICENSE" title="license"><img alt='' src="https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square" /></a>
@@ -98,16 +98,16 @@ export default function Project() {
                         <div className='card border-secondary'>
                             <img alt='' className='card-img-top' src={projectPortfolio} />
                             <div class="text-white card-img-overlay">
-                                <p className='h2 bg-dark md-title text-light card-title'>
+                                <p className='h2 card-dark md-title text-light card-title'>
                                     {t("project.portfolio.title")}
                                 </p>
-                                <a href="https://github.com/abass-dev/portfolio" class="btn mt-2 text-primary btn-primary-outline btn-sm bg-dark"><i className='fa fa-github'></i> Code</a>
+                                <a href="https://github.com/abass-dev/portfolio" class="btn mt-2 text-primary btn-primary-outline btn-sm card-dark"><i className='fa fa-github'></i> Code</a>
                                 {" "}
                                 <a href="https://github.com/abass-dev/portfolio/blob/master/LICENSE" title="license"><img alt='' src="https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square" /></a>
                                 {" "}
                                 <a href="https://github.com/abass-dev/portfolio/issues"><img alt="Issues" src="https://img.shields.io/github/issues/abass-dev/portfolio?color=0088ff" /></a>
                             </div>
-                            <div className='card-body bg-dark text-light'>
+                            <div className='card-body card-dark text-light'>
                                 <i className='fw-bold fa fa-code'></i>
                                 <span className='fw-lighter'>
                                     {" "}
