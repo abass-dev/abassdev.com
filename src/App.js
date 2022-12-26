eab31ea74c37604d337f1814dcef4dd18cca39c7
abass-dev <abassthedev@gmail.com>
2022-12-26 16:21:43 +0000

    remove trans folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import Home from "./Component/Home/Home";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "G-N8ZTB9NPSP",
+};
+
+TagManager.initialize(tagManagerArgs);
 
 export default function App() {
   return <Home />;
