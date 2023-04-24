2e7b6a7dc1695f67a0c413e7ffa59684d6814930
abass-dev <abassthedev@gmail.com>
2023-04-24 17:54:57 +0100

    fix: birthday date

--- src/Component/Helpers/birthday.js
+++ src/Component/Helpers/birthday.js
@@ -6,10 +6,9 @@ export default function birthday(date) {
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
 
   const diffYears = Math.floor(diffDays / 365)
-  const diffMonths = Math.floor((diffDays - diffYears * 365) / 30)
+  const diffMonths = Math.floor((diffDays % 365) / 30)
   const diffDaysRemaining = diffDays - diffYears * 365 - diffMonths * 30
 
   const formattedDiff = `${diffYears} years and ${diffMonths} months old today.`
-
   return formattedDiff
 }
