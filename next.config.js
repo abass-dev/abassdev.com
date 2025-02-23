b3fa5372875688466fbada5e9ea89a8c45f27026
Abass Dev <abassthedev@gmail.com>
2025-02-23 08:04:27 +0100

    fix blog url

--- next.config.js
+++ next.config.js
@@ -4,6 +4,30 @@ const withNextIntl = createNextIntlPlugin('./app/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/blog/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'abassdev.com',
+          },
+        ],
+        destination: 'http://abassdev.com/blog/:path*'
+      },
+      {
+        source: '/blog/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.abassdev.com',
+          },
+        ],
+        destination: 'http://abassdev.com/blog/:path*'
+      }
+    ]
+  },
     images: {
         domains: ['abassdev.com'],
     },
